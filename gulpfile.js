d4bf4e1ab1b36751557311c1cf2971d2aeede1ea
Leonardo Carapecos <leonardocarapecosdev@gmail.com>
2023-08-29 21:40:36 -0300

    setup do projeto

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function styles(){
+    return gulp.src('./src/styles/*.scss')
+    .pipe(sass({outputStyle:'compressed'}))
+    .pipe(gulp.dest('./dist/css'));
+}
+
+function saveAutomatico(){
+    gulp.watch('./src/styles/*scss', gulp.parallel(styles))
+}
+
+
+
+exports.default = styles;
+exports.watch = saveAutomatico;
